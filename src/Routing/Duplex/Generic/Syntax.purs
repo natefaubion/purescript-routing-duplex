9d765b6eda8ad594236bb37792091be04a470a86
Nathan Faubion <nathan@n-son.com>
2018-11-15 22:40:26 -0800

    More flexible syntax constraints

--- src/Routing/Duplex/Generic/Syntax.purs
+++ src/Routing/Duplex/Generic/Syntax.purs
@@ -3,7 +3,7 @@ module Routing.Duplex.Generic.Syntax where
 import Prelude
 
 import Data.Generic.Rep (Argument, Product)
-import Routing.Duplex (class RouteDuplexParams, RouteDuplex, RouteDuplex', params, path)
+import Routing.Duplex (class RouteDuplexParams, RouteDuplex, RouteDuplex', params, prefix, suffix)
 import Routing.Duplex.Generic (class GRouteDuplexCtr, gRouteDuplexCtr, product)
 
 class GSep a b c | a b -> c where
@@ -11,12 +11,17 @@ class GSep a b c | a b -> c where
 
 instance gsepStringString ::
   GSep String String Unit where
-  gsep a b = path a $ path b $ pure unit
+  gsep a b = prefix a $ prefix b $ pure unit
 else
 instance gsepStringRoute ::
   GRouteDuplexCtr a b =>
   GSep String (RouteDuplex a a) b where
-  gsep a = path a <<< gRouteDuplexCtr
+  gsep a = prefix a <<< gRouteDuplexCtr
+else
+instance gsepRouteString ::
+  GRouteDuplexCtr a b =>
+  GSep (RouteDuplex a a) String b where
+  gsep = suffix <<< gRouteDuplexCtr
 else
 instance gsepProduct ::
   GRouteDuplexCtr b c =>
@@ -25,11 +30,17 @@ instance gsepProduct ::
 
 infixr 1 gsep as /
 
-gquery :: forall a r1 r2.
-  RouteDuplexParams r1 r2 =>
-  RouteDuplex' a ->
-  { | r1 } ->
-  RouteDuplex' (Product (Argument a) (Argument { | r2 }))
-gquery a b = product a (params b)
+class GParams a b c | a b -> c where
+  gparams :: a -> b -> RouteDuplex' c
 
-infix 8 gquery as ?
+instance gparamsString ::
+  RouteDuplexParams r1 r2 =>
+  GParams String { | r1 } { | r2 } where
+  gparams a = prefix a <<< params
+else
+instance gparamsRoute ::
+  RouteDuplexParams r1 r2 =>
+  GParams (RouteDuplex a a) { | r1 } (Product (Argument a) (Argument { | r2 })) where
+  gparams a = product a <<< params
+
+infix 8 gparams as ?
